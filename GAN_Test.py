fe0022a289c26947681dfe6a38fa605b404e6407
Peter Bautista <peter.g.bautista@gmail.com>
2020-08-26 22:53:03 -0700

    Added data generation after training

--- GAN_Test.py
+++ GAN_Test.py
@@ -13,7 +13,6 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 #filename = input("Enter the filename: ")
 dataset = pd.read_csv("./Datasets/StudentData_121.csv")
-
 real, percentage, letter = splitKeywords(dataset,"Real","Percentage","Letter","sadfasd")
 dataset = dataset[percentage]
 
@@ -22,6 +21,8 @@ cleanDataset(dataset)
 cleanDataName(dataset)
 
 features = getHighestCorrFeatures(dataset)
+features = getFeatures(dataset.columns.values,"Quiz", "Midterm exam total", "Assignment")
+print(features)
 
 education_data = (dataset[features]).sort_index(axis=1)
 education_data = (education_data.replace(to_replace="-",value=0.0)).astype("float64")
@@ -34,16 +35,14 @@ education_data.to_csv("./Processed_Data/clean_data.csv")
 # Randomly apply labels to the dataset for testing purposes
 GAN_NN = GAN(education_data, features, realData=False, filepath="./Processed_Data/clean_data.csv")
 
-
-
 # Initialize models for the GAN
 D_Network = discriminatorModel(education_data)
 G_Network = generatorModel(education_data)
 
 GAN_NN.initializeNetworks(generator=G_Network, discriminator=D_Network)
 print("Initial generation", GAN_NN.generateFakeData(size=1))
-test = GAN_NN.train_network(epochs=30,batch_size=16, view_history=True)
+test = GAN_NN.train_network(epochs=600,batch_size=16, view_history=True)
 print("Final generation", GAN_NN.generateFakeData(size=1))
 
-
-#test.to_csv("./Datasets/GeneratedSet.csv")
+d = GAN_NN.generateFakeData(size=100)
+d.to_csv("./GeneratedData.csv")
